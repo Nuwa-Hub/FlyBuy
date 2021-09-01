68a4c4ac42ba2aea050d6f86681c501c65ca1906
iamakash <akash_tharuka@yahoo.com>
2021-09-01 21:06:43 +0530

    verifyEmail and emailVerified php files

---
+++ emailVerified.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Email Verified</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/styles_verifyEmail.css" />
+    </head>
+
+    <body>
+        <div class="container">
+            <!-- <img class="mail-icon" src=""> -->
+            <h2>Verification Successful!</h2>
+            <a class="loginLink" href="#">Login To Your Account</a>
+        </div>
+    </body>
+</html>
---
+++ verifyEmail.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Verify email</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="stylesheet" href="/styles_verifyEmail.css" />
+    </head>
+
+    <body class="main-body">
+
+        <div class="container">
+
+            <!-- <img class="mail-icon" src="/envelope_user.png"> -->
+            
+            <h2>Please Verify Your Email Address</h2>
+
+            <span>Haven't Received Your Email Yet?</span>
+
+            <a href="#" class="sendAgainLink">Click Here to Re-send</a>
+            
+            <a class="loginLink" href="/">Login To Your Account</a>
+
+        </div>
+
+    </body>
+
+</html>
