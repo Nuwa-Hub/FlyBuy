71196455cdfef2f609412fd94889176144a9532b
Nuwandeshapriya <79309450+Nuwandeshapriya@users.noreply.github.com>
2021-09-02 16:47:33 +0530

    fdfdf

--- loginSignup.php
+++ loginSignup.php
@@ -1,90 +1,169 @@
-<?php 
+<?php
 include 'templates/customer.php';
 include 'templates/seller.php';
 include 'database/db_connection.php';
+require('user_validator.php');
 
 session_start();
 
 
 
-// define variables and set to empty values
-$passwordErr = $emailErr =$usernameErr=$confirmPswErr=  "";
-$password = $email =$username=$confirmPsw=$userType= "";
+$errors = [];
 
-if (isset($_POST["submitLogin"])) {
-  if (empty($_POST["password"])) {
-    $passwordErr = "password is required";
-  } else {
-    $password = ($_POST["password"]);
-  }
-
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = ($_POST["email"]);
-  }
-  echo $email;
-  echo $password;
- 
+function checknone($arr)
+{
+    foreach ($arr as $ele) {
+        if ($ele != 'none') {
+            return false;
+        }
+    }
+    return true;
 }
 
-if(isset($_POST["submitSign"])){
-    if (empty($_POST["password"])) {
-        $passwordErr = "password is required";
-      } else {
-        $password = ($_POST["password"]);
-      }
-    
-      if (empty($_POST["email"])) {
-        $emailErr = "Email is required";
-      } else {
-        $email = ($_POST["email"]);
-      }
-      if (empty($_POST["username"])) {
-        $usernameErr = "username is required";
-      } else {
-        $username = ($_POST["username"]);
-      }
-      if (empty($_POST["confirmPsw"])) {
-        $confirmPswErr = "confirm password is required";
-      } else {
-        $confirmPsw = ($_POST["confirmPsw"]);
-      }
-
-    
-    if (!empty($_POST["password"]) && !empty($_POST["email"]) && !empty($_POST["username"]) && !empty($_POST["confirmPsw"])){
+if (isset($_POST['submitSignup'])) {
 
 
-                if  ($_POST["userType"]=="seller"){
-                
-                $sql = "INSERT INTO sellers (username,email,password) VALUES ('$username','$password','$email')";
 
-                if ($conn->query($sql) === TRUE) {
-                        echo "New record created successfully";
-                } else {
-                        echo "Error: " . $sql . "<br>" . $conn->error;
-                }
-                    
-                }
-                else{
-                    
 
-                $sql = "INSERT INTO customers(username,email,password) VALUES ('$username','$password','$email')";
 
-                if ($conn->query($sql) === TRUE) {
-                        echo "New record created successfully";
-                } else {
-                        echo "Error: " . $sql . "<br>" . $conn->error;
-                }
-                }
 
-                $conn->close();
-                //   echo $email;
-                //   echo $password;
-                //   echo $confirmPsw;
-                //   echo $username;
+
+if ($_POST['userType'] == "buyer") {
+
+        //write query for all users
+        $sql = 'SELECT * FROM customers';
+
+        //make query and get result
+        $result = mysqli_query($conn, $sql);
+
+        //fetch the resulting rows as an array
+        $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+        // validate entries
+        $validation = new UserValidator($_POST, $users, "buyer");
+        $errors = $validation->validateForm('signup');
+
+        //array_filter($errors)
+
+        if (checknone($errors)) {
+
+            $sql = "INSERT INTO customers (username,email,password,telNo,address) VALUES ('$_POST[username]','$_POST[email]','$_POST[password]','$_POST[telNo]','$_POST[address]')";
+            $errors = [];
+            if ($conn->query($sql) === TRUE) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
             }
+        } else {
+            print_r(array_values($errors));
+        }
+    } else {
+
+        //write query for all users
+        $sql = 'SELECT * FROM sellers';
+
+        //make query and get result
+        $result = mysqli_query($conn, $sql);
+
+        //fetch the resulting rows as an array
+        $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+        // validate entries
+        $validation = new UserValidator($_POST, $users, "seller");
+        $errors = $validation->validateForm('signup');
+
+
+
+        if (checknone($errors)) {
+
+            $sql = "INSERT INTO sellers (username,email,password,telNo,address,storeName) VALUES ('$_POST[username]','$_POST[email]','$_POST[password]','$_POST[telNo]','$_POST[address]','$_POST[storeName]')";
+            $errors = [];
+            if ($conn->query($sql) === TRUE) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+        } else {
+            print_r(array_values($errors));
+        }
+    }
+} 
+
+if (isset($_POST['submitLogin'])){
+
+
+       if ($_POST['userType'] == "buyer") {
+
+        //write query for all users
+        $sql = 'SELECT * FROM customers';
+
+        //make query and get result
+        $result = mysqli_query($conn, $sql);
+
+        //fetch the resulting rows as an array
+        $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+        // validate entries
+        $validation = new UserValidator($_POST, $users, "buyer");
+        $errors = $validation->validateForm('login');
+
+        //array_filter($errors)
+
+        if (checknone($errors)) {
+             echo "c";
+          //  header('Location: home.php');
+        } else {
+            print_r(array_values($errors));
+        }
+    } else {
+
+        //write query for all users
+        $sql = 'SELECT * FROM sellers';
+
+        //make query and get result
+        $result = mysqli_query($conn, $sql);
+
+        //fetch the resulting rows as an array
+        $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+        // validate entries
+        $validation = new UserValidator($_POST, $users, "seller");
+        $errors = $validation->validateForm('login');
+
+
+
+        if (checknone($errors)) {
+                echo "c";
+          //  header('Location: home.php ');
+        } else {
+            print_r(array_values($errors));
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
 
 <html lang="en">
@@ -93,32 +172,27 @@ if(isset($_POST["submitSign"])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link 
-        rel="stylesheet" 
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" 
-        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" 
-        crossorigin="anonymous" 
-    />
 
-    <link rel="stylesheet" href="./css/styles_signinLogin.css">
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+
+     <link rel="stylesheet" href="./css/styles_signinLogin.css">  
+
     <title>SignIn and SignUp</title>
 </head>
 
 <body>
 
     <main>
-        
+
         <div class="column">
 
             <div class="container">
 
                 <div class="forms-container">
-        
+
                     <div class="signin-signup sign-in">
 
-                        <form class="sign-in-form">
+                        <form class="sign-in-form" method="post" action="loginSignup.php">
 
                             <h2 class="title">Sign in</h2>
 
@@ -126,7 +200,7 @@ if(isset($_POST["submitSign"])){
                                 <i class="fas fa-envelope"></i>
                                 <input name="email" type="text" placeholder="Email" class="email">
                                 <i class="fas fa-exclamation-circle tooltip">
-                                <span class="error">Error Message</span>
+                                    <span class="error">Error Message</span>
                                 </i>
                                 <i class="fas fa-check-circle"></i>
                             </div>
@@ -137,22 +211,34 @@ if(isset($_POST["submitSign"])){
                                 <a href="#"><small class="forgotPsw">forgotten password?</small></a>
                                 <i class="fas fa-eye togglePassword"></i>
                                 <i class="fas fa-exclamation-circle tooltip">
-                                <span class="error">Error Message</span>
+                                    <span class="error">Error Message</span>
                                 </i>
                                 <i class="fas fa-check-circle"></i>
                             </div>
-                          
-                             <button class="btn solid login" name="submitLogin">
+
+
+                            <div class="input-field radio">
+                                <input type="radio" class="radioBtn buyer" name="userType" value="buyer" onchange="removeField()" checked>
+                                <label for="radio">Buyer</label>
+                                <input type="radio" class="radioBtn seller" name="userType" value="seller" onchange="addField()">
+                                <label for="radio">Seller</label>
+                                <i class="fas fa-exclamation-circle tooltip">
+                                    <small class="tooltip-text">Error Message</small>
+                                </i>
+                                <i class="fas fa-check-circle"></i>
+                            </div>
+
+                            <button class="btn solid login" name="submitLogin">
                                 <span class="buttonText">Login</span>
-                            </button> 
-                            
+                            </button>
+
                         </form>
 
                     </div>
-        
+
                     <div class="signin-signup sign-up">
 
-                        <form class="sign-up-form">
+                        <form class="sign-up-form" method="post" action="loginSignup.php">
 
                             <h2 class="title">Sign up</h2>
 
@@ -176,7 +262,7 @@ if(isset($_POST["submitSign"])){
 
                             <div class="input-field">
                                 <i class="fas fa-mobile-alt"></i>
-                                <input name="phoneNumber" type="tel" placeholder="Phone Number" class="phone">
+                                <input name="telNo" type="tel" placeholder="Phone Number" class="phone">
                                 <i class="fas fa-exclamation-circle tooltip">
                                     <small class="tooltip-text">Error Message</small>
                                 </i>
@@ -185,7 +271,7 @@ if(isset($_POST["submitSign"])){
 
                             <div class="input-field">
                                 <i class="fas fa-map-marked-alt"></i>
-                                <input name="address" type="text" placeholder="Address" class="address">
+                                <input name="address" type="text" placeholder="Address Ex:- No.20,city,county" class="address">
                                 <i class="fas fa-exclamation-circle tooltip">
                                     <small class="tooltip-text">Error Message</small>
                                 </i>
@@ -232,18 +318,18 @@ if(isset($_POST["submitSign"])){
                                 <i class="fas fa-check-circle"></i>
                             </div>
 
-                            <button class="btn solid signup" name="submitSign">
+                            <button class="btn solid signup" name="submitSignup">
                                 <span class="buttonText">Sign Up</span>
                             </button>
 
                         </form>
 
                     </div>
-        
+
                 </div>
-        
+
                 <div class="panels-container">
-        
+
                     <div class="panel left-panel">
                         <div class="content">
                             <h3>One of Us?</h3>
@@ -256,7 +342,7 @@ if(isset($_POST["submitSign"])){
                             <img src="user.png" alt="user">
                         </div>
                     </div>
-        
+
                     <div class="panel right-panel">
                         <div class="content">
                             <img src="user.png" alt="user">
@@ -264,27 +350,27 @@ if(isset($_POST["submitSign"])){
                             <p>
                                 Join us and enjoy the services
                             </p>
-                            <button class="btn transparent" id="sign-up-button" name="submitign">
+                            <button class="btn transparent" id="sign-up-button">
                                 <span class="buttonText">Sign up</span>
                             </button>
                         </div>
                     </div>
-        
+
                 </div>
-        
+
             </div>
 
         </div>
 
     </main>
-    
+
 </body>
 
 <script>
     // toggle login and signup animation
-    const sign_in_btn   = document.querySelector('#sign-in-button');
-    const sign_up_btn   = document.querySelector('#sign-up-button');
-    const container     = document.querySelector('.container');
+    const sign_in_btn = document.querySelector('#sign-in-button');
+    const sign_up_btn = document.querySelector('#sign-up-button');
+    const container = document.querySelector('.container');
 
     sign_up_btn.addEventListener('click', () => {
         container.classList.add('sign-up-mode');
@@ -295,10 +381,10 @@ if(isset($_POST["submitSign"])){
     });
 
     // toggle password view---------------------------------------------------------------
-    let toggleView      = document.querySelectorAll('.togglePassword');
+    let toggleView = document.querySelectorAll('.togglePassword');
 
-    function togglePasswordView(toggleView){
-        for (let i = 0; i < toggleView.length; i++){
+    function togglePasswordView(toggleView) {
+        for (let i = 0; i < toggleView.length; i++) {
             let pswField = toggleView[i].parentElement.querySelector('input');
 
             toggleView[i].addEventListener('click', (event) => {
@@ -311,23 +397,22 @@ if(isset($_POST["submitSign"])){
         }
     };
 
-    if (toggleView){
+    if (toggleView) {
         togglePasswordView(toggleView);
     }
 
     // add extra seller information
     let radioBtns = document.querySelectorAll('.radioBtn');
     let storeName = document.querySelector('.input-field.store');
-    
-    function addField(){
+
+    function addField() {
         storeName.classList.remove('remove');
 
     }
 
-    function removeField(){
+    function removeField() {
         storeName.classList.add('remove');
     }
-
 </script>
 
 </html>
