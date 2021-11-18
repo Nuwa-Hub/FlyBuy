1a60843d17d449c32e95abf5d3f4662efb7871e5
Nuwandeshapriya <79309450+Nuwandeshapriya@users.noreply.github.com>
2021-11-18 16:36:02 +0530

    h

--- view/homepage.php
+++ view/homepage.php
@@ -24,7 +24,7 @@ else{
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Project cosmos</title>
-    <link rel="stylesheet" href="../css/style-home.css">
+     <link rel="stylesheet" href="../css/style-home.css"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
   </head>
