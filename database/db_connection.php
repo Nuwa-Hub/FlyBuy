683a4080400c26e21f338e3621cb80cc18cd0cd0
Nuwandeshapriya <79309450+Nuwandeshapriya@users.noreply.github.com>
2021-11-18 11:47:50 +0530

    erret

--- database/db_connection.php
+++ database/db_connection.php
@@ -6,13 +6,8 @@
   $db           = "flybuy";
 
   // Create connection
-<<<<<<< HEAD
    $conn         = mysqli_connect($servername, $username, $password, $db);
   //$conn = mysqli_connect('127.0.0.1:3310', 'Kalana', '0000', 'flybuy');
-=======
-  $conn         = mysqli_connect($servername, $username, $password, $db);
-  // $conn = mysqli_connect('127.0.0.1:3310', 'Kalana', '0000', 'flybuy');
->>>>>>> 44ab5016a491c806f07004ded56c2704a7caa24d
 
   // Check connection
   if (!$conn) {
