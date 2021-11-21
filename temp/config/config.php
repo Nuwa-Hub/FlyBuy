00cf20bd152dba9ce3e6f3af49a3bacdc4da85c0
Kalana99 <kalanarajika99@gmail.com>
2021-11-21 17:25:26 +0530

    init mvc

---
+++ temp/config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+// $conn = mysqli_connect('127.0.0.1:3310', 'Kalana', '0000', 'flybuy');
+//database params
+define('DB_HOST', '127.0.0.1:3310');
+define('DB_USER', 'Kalana');
+define('DB_PASS', '0000');
+define('DB_NAME', 'flybuy');
+
+//app root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//url root (dynamic links)
+define('URLROOT', '127.0.0.1/mvc');
+
+//sitename
+define('SITENAME', 'MVC Tute');
+
+?>
---
+++ temp/views/pages/index.php
@@ -0,0 +1,5 @@
+<?php
+
+var_dump($data['users'][0]->email);
+
+?>
