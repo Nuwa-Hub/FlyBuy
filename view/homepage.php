5423c3807a637e65d972e16b8959b2fc631b164b
RansikaC99 <rumal.19@cse.mrt.ac.lk>
2021-11-22 11:44:34 +0530

    ljbl

--- view/homepage.php
+++ view/homepage.php
@@ -22,7 +22,13 @@ $user  = mysqli_fetch_all(mysqli_query($conn, "SELECT * FROM buyers WHERE email 
 
 
 $products = mysqli_fetch_all(mysqli_query($conn, "SELECT * FROM  products"), MYSQLI_ASSOC);
-//}
+for($i = 0; $i < count($products); $i++){
+  $seller_id = $products[$i]['seller_id'];
+   $seller = mysqli_fetch_all(mysqli_query($conn, "SELECT * FROM sellers WHERE seller_id = '$seller_id' LIMIT 1"), MYSQLI_ASSOC)[0];
+  $products[$i]['seller'] = $seller;
+  }
+ }
+
 
 
 
