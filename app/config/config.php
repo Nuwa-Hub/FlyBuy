a2bcc3b8003faa41fa365375257f0e6ffd49c4a6
tharukaeaa <akash.19@cse.mrt.ac.lk>
2021-11-24 14:22:12 +0530

    asd

---
+++ app/config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+// $conn = mysqli_connect('127.0.0.1:3310', 'Kalana', '0000', 'flybuy');
+// define('URLROOT', '127.0.0.1/FlyBuy');
+
+//database params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'flybuy');
+
+//app root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL ROOT - https can be removed (Dynamic links)
+define('URLROOT', 'http://localhost/Project/FlyBuy');
+
+//site name
+define('SITENAME', 'COSMOS-FLYBUY');
+
+?>
