1fc22c2641dc6eeb0557b54c6868ed8c1c27e45a
RansikaC99 <rumal.19@cse.mrt.ac.lk>
2021-11-30 12:23:49 +0530

    SDF

--- database/db_connection.php
+++ database/db_connection.php
@@ -7,11 +7,8 @@
 
   // Create connection
 
-<<<<<<< HEAD
-  $conn         = mysqli_connect($servername, $username, $password, $db);
-=======
      $conn         = mysqli_connect($servername, $username, $password, $db);
->>>>>>> efb1d50f9dfb010bf6aab446cbdd3c7ee00d04dd
+
 
   //$conn = mysqli_connect('127.0.0.1:3310', 'Kalana', '0000', 'flybuy');
 
