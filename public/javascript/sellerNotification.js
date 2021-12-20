63ed7a99081d0aeead55f3e9c1c292633c674cf4
Nuwandeshapriya <79309450+Nuwandeshapriya@users.noreply.github.com>
2021-12-20 17:06:10 +0530

    d

--- public/javascript/sellerNotification.js
+++ public/javascript/sellerNotification.js
@@ -1,32 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
     var seller_id = document.querySelector('.seller-id.notify').value;
 
-    function load_unseen_notification(view = ''){
+    function load_unseen_notification(view = '') {
 
         $.ajax({
-            url:"http://localhost/Project/FlyBuy/UserController/getNotificationCount",
-            method:"POST",
-            data:{
-                view : view,
-                seller_id : seller_id
+            url: "http://localhost/Project/FlyBuy/UserController/getNotificationCount",
+            method: "POST",
+            data: {
+                view: view,
+                seller_id: seller_id
             },
-            dataType:"json",
-            success:function(data){
+            dataType: "json",
+            success: function(data) {
 
-                if(data.rowCount > 0){
+                if (data.rowCount > 0) {
                     $('.badge').html(data.rowCount);
-                }
-                else{
-                    document.querySelector('.badge').style.opasity = "0";
+                } else {
+                    document.querySelector('.badge').style.opacity = "0";
                 }
             }
         });
     }
-    
+
     load_unseen_notification();
 
-    setInterval(function(){
+    setInterval(function() {
         load_unseen_notification();
     }, 1000);
 
