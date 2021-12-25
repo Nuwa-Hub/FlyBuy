fb6338524f5df6fb89e422f31828bb03be79a2db
Nuwandeshapriya <79309450+Nuwandeshapriya@users.noreply.github.com>
2021-12-25 14:29:57 +0530

    xz

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -174,24 +174,22 @@ class UserController extends Controller {
 
         $buyer_id = $_POST['buy_id'];
         $cart = [];
+        $order = [];
 
         foreach ($_SESSION['cartarr'] as $product) {
 
             $seller_id = $product->seller_id;
             $cart[$seller_id][$product->item_id] = $product->amount[1];
-            $cart[$seller_id]['order_price'] += $product->price * $product->amount[1];
         }
 
         $this->buyerModel->saveCart($buyer_id, $cart);
 
         foreach ($cart as $seller_id => $order) {
             $this->sellerModel->saveNotification($buyer_id, $seller_id, $order);
-
         }
 
          $_SESSION['cartarray'] = $_SESSION['cartarr'];
          $_SESSION['cartarr']=[];
-
     }
 
     public function getNotificationCount(){
@@ -207,10 +205,11 @@ class UserController extends Controller {
         echo json_encode($data);
     }
 
-    public function markNotfication(){
-        
+    public function markNotficationAsRead(){
+
         $id = $_POST['notify_id'];
-        $this->sellerModel->markNotificationById($id);
+        
+        $this->sellerModel->markAsReadById($id);
     }
 
     public function logout(){
@@ -218,11 +217,10 @@ class UserController extends Controller {
         if(isset($_POST['submitLogout'])){
 
             if (isset($_COOKIE['user_login'])) {
+
                 unset($_COOKIE['user_login']); 
                 setcookie('user_login', null, -1, '/');
             }
-
-            session_destroy ();
             
             header('location: ' . URLROOT . '/PageController/loginSignup');
         }
