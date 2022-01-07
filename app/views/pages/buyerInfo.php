ce4db4ac8ff8dc64f26ee76c811ae0d26a93e5a2
RansikaC99 <rumal.19@cse.mrt.ac.lk>
2022-01-07 23:17:02 +0530

    fgf

--- app/views/pages/buyerInfo.php
+++ app/views/pages/buyerInfo.php
@@ -39,14 +39,16 @@
     <div class="orders">
       <h1 style="color:white;">Order history</h1>
       <div class="orderList">
+      <?php foreach ($data['products'] as $product): ?>
         <div class="item">
           <img src="<?php echo URLROOT; ?>/public/img/kottu mee.png"style="width:100px;height:100px;">
-          <h3 id="name" style="color:#303030;font-family:cursive;font-size:20px;">Prima kottu mee</h3>  
+          <h3 id="name" style="color:#303030;font-family:cursive;font-size:20px;"><?php echo $product->itemName; ?></h3>  
           <h3 id="store" style="color:#303030;font-family:cursive;font-size:20px;">RC STORES</h3>
           <h3 id="date" style="color:#303030;font-family:cursive;font-size:20px;">Placed on 31 Dec 2021</h3>   
-          <h3 id="quantity" style="color:#303030;font-family:cursive;font-size:20px;">Quantity:-2</h3>   
-          <h3 id="price" style="color:blue;font-family:cursive;font-size:25px;">Total: Rs.890</h3>    
+          <h3 id="quantity" style="color:#303030;font-family:cursive;font-size:20px;"><?php echo $product->itemName; ?></h3>   
+          <h3 id="price" style="color:blue;font-family:cursive;font-size:25px;"><?php echo "Rs. ".$product->price; ?></h3>    
         </div>
+        <?php endforeach; ?>
       </div>
     </div>
   </div>
@@ -78,13 +80,13 @@
 <script src="<?php echo URLROOT; ?>/public/javascript/popupFormValidation.js"></script>
 
 <script src="<?php echo URLROOT; ?>/public/javascript/jquery.min.js"></script>
-<script src="<?php echo URLROOT; ?>/public/javascript/star.js"></script>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js"></script>
 <script src="<?php echo URLROOT ?>/public/javaScript/shoppingCart.js"></script>
 
-<script src="<?php echo URLROOT; ?>/public/javascript/particles.js"></script>
-<script src="<?php echo URLROOT; ?>/public/javascript/snow.js"></script>
-<script src="<?php echo URLROOT; ?>/public/javascript/slideshow.js"></script>
+
+
+
 
 </html>
