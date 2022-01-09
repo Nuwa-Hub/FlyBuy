cd06597a35554cc210bc3d56fb18b59201b1a119
Kalana99 <kalanarajika99@gmail.com>
2022-01-09 16:57:40 +0530

    update ratings at the end of the day

--- public/javascript/homePage.js
+++ public/javascript/homePage.js
@@ -103,4 +103,21 @@ function searchElement() {
     }
 }
 
+//update seller ratings end of the day
 
+var now = new Date();
+var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 0, 0) - now;
+
+if (millisTill10 < 0) {
+     millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
+}
+
+setTimeout(function(){
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost/Project/FlyBuy/UserController/updateRating",
+        success: function(result = '') {}
+    });
+}, 
+millisTill10);
